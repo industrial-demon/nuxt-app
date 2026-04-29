c1d80462178ed62924892349d1fb2998bc8fce7f
motograter <drew.naumenko@gmail.com>
2026-04-29 21:55:14 +0300

    deploy files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  nitro: {
+    preset: 'github_pages'
+  },
+  app: {
+    baseURL: '/nuxt-app/' // Replace with your repository name
+  },
   devtools: { enabled: true }
 })
